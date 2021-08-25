ab8596c6710ed8cb2d7779fee366d1c2daefa0ca
PLW <peilingwu0101@gmail.com>
2021-08-25 17:04:59 +0200

    created database to save frontend currency threshold price input

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,44 @@
-const express = require('express')
-var cors = require('cors')
- 
-const axios = require('axios')
-const app = express()
-const port = 8080
-app.use(cors())
+const express = require("express");
+var cors = require("cors");
+const fs = require("fs");
+const axios = require("axios");
+const app = express();
+const port = 8080;
+app.use(cors());
+app.use(express.json()); //Used to parse JSON bodies
+app.use(express.urlencoded()); //Parse URL-encoded bodies
 
-app.get('/prices', async(req, res) => {
-//TODO the coin price api call
-    try {
-      const response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=20&convert=EUR',{
+app.get("/prices", async (req, res) => {
+  //TODO the coin price api call
+  try {
+    const response = await axios.get(
+      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=20&convert=EUR",
+      {
         headers: {
           "X-CMC_PRO_API_KEY": "3484a1fc-bf64-4206-9f73-2592b24719df",
         },
-      });
-      console.log(response);
-      res.json(response.data)
-    } catch (error) {
-      console.error(error);
-      res.json(error)
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.json(error);
+  }
+});
 
+app.post("/setthreshhold", async (req, res) => {
+  try {
+    const dir=`currencyThreshold`
+    if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir, { recursive: true });
     }
-})
+    const fileWriteResponse = fs.writeFileSync(`./${dir}/${req.body.currency}_threshold_price.txt`, req.body.value);
+    res.json({ message: "success" });
+  } catch (err) {
+    console.error(err);
+  }  
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ function App() {
   }, []);
   console.log(data);
   //TODO save the trheshold value somewhere
+  // send the number while typing or on button click to the backend and store it in a file name like the currency
   //TODO make a interval which checks the price all 10 minues
   //TODO when the threshold is undergone trigger a notification (mail/telegram)
   return (
@@ -28,7 +29,6 @@ function App() {
         <thead>
           <tr>
             <th scope="col">Currency</th>
-          
             <th scope="col">Price</th>
             <th scope="col">Threshold</th>
           </tr>
@@ -37,18 +37,45 @@ function App() {
           {data.map((currency) => (
             <tr>
               <th scope="row">{currency.name}</th>
-              <td><code>{currency.quote.EUR.price.toFixed(5)}</code></td>
-              <td><div className="input-group mb-3">
-                <span className="input-group-text">$</span>
-                <input
-                  type="text"
-                  className="form-control"
-                  aria-label="Amount (to the nearest dollar)"
-                />
-                <span className="input-group-text">.00</span>
-              </div></td>
-              
-              
+              <td>
+                <code>{currency.quote.EUR.price.toFixed(5)}</code>
+              </td>
+              <td>
+                <div className="input-group mb-3">
+                  <span className="input-group-text">$</span>
+                  <form
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                      console.log(e.target.elements[0].value);
+                      //TODO perform a post request to a new backend endpoint
+                      fetch(`http://localhost:8080/setthreshhold`, {
+                        method: "POST",
+                        body: JSON.stringify({
+                          currency: currency.name,
+                          value: e.target.elements[0].value
+                        }),
+                        headers: {
+                          "content-type": "application/json"
+                        }
+                      })
+                        .then((res) => res.json())
+                        .then((res) => {
+                          console.log(res);
+                        });
+                    }}
+                  >
+                    <input
+                      type="text"
+                      className="form-control"
+                      aria-label="Amount (to the nearest dollar)"
+                    />
+                    <button className={`btn btn-success`} type="submit">
+                      Save
+                    </button>
+                  </form>
+                  <span className="input-group-text">.00</span>
+                </div>
+              </td>
             </tr>
           ))}
         </tbody>
