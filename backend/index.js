573fd0e63564d204d541ba47022de8c716f890bc
PLW <peilingwu0101@gmail.com>
2021-08-30 19:50:23 +0200

    compare threshold price and send notification

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 var cors = require("cors");
 const fs = require("fs");
@@ -8,43 +8,83 @@ const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json()); //Used to parse JSON bodies
 app.use(express.urlencoded()); //Parse URL-encoded bodies
-const dir=`currencyThreshold`
+const dir = `${__dirname}/currencyThreshold`;
+
+const getPricesFromAPI = async () => {
+  const response = await axios.get(
+    "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=20&convert=EUR",
+    {
+      headers: {
+        "X-CMC_PRO_API_KEY": process.env.API_KEY,
+      },
+    }
+  );
+  return response;
+};
+
+const getSavedThresholds = async () => {
+  let currencies = [];
+  fs.readdirSync(dir).forEach((file) => {
+    const content = fs.readFileSync(dir + `/` + file, { encoding: "utf-8" });
+    currencies.push({
+      currency: file.replace("_threshold_price.txt", ""),
+      value: content,
+    });
+  });
+  return currencies;
+};
 
 app.get("/prices", async (req, res) => {
   //TODO the coin price api call
   try {
-    const response = await axios.get(
-      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=20&convert=EUR",
-      {
-        headers: {
-          "X-CMC_PRO_API_KEY": process.env.API_KEY,
-        },
-      }
-    );
-    let currencies = []
-    fs.readdirSync(dir).forEach(file => {
-      const content = fs.readFileSync(dir + `/` + file, {encoding: 'utf-8'})
-      currencies.push({currency: file.replace("_threshold_price.txt",""), value: content})
-    });
-    res.json({currencies: currencies, prices: response.data});
+    const response = await getPricesFromAPI();
+    const currencies = await getSavedThresholds();
+
+    res.json({ currencies: currencies, prices: response.data });
   } catch (error) {
     console.error(error);
     res.json(error);
   }
 });
-console.log(process.env.API_KEY);
 
 app.post("/setthreshhold", async (req, res) => {
   try {
-    if (!fs.existsSync(dir)){
+    if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir, { recursive: true });
     }
-    const fileWriteResponse = fs.writeFileSync(`./${dir}/${req.body.currency}_threshold_price.txt`, req.body.value);
+    const fileWriteResponse = fs.writeFileSync(
+      `${dir}/${req.body.currency}_threshold_price.txt`,
+      req.body.value
+    );
     res.json({ message: "success" });
   } catch (err) {
     console.error(err);
-  }  
+  }
 });
+
+setInterval(async () => {
+  const response = await getPricesFromAPI();
+  const thresholds = await getSavedThresholds();
+  thresholds.map(async (localSavedCurrency) => {
+    const cryptoToCompare = response.data.data.find(
+      (apiCurr) => localSavedCurrency.currency === apiCurr.name
+    );
+    
+    if (cryptoToCompare.quote.EUR.price < Number(localSavedCurrency.value)) {
+      console.log(localSavedCurrency.currency + " you shall buy now!!!!");
+      try {
+        const response = await axios.post(
+          `https://api.telegram.org/${process.env.TELEGRAM_API_KEY}/sendMessage`,{chat_id: 1940166204, text: localSavedCurrency.currency + " you shall buy now!!!!"});
+        
+      } catch (error) {
+        console.log(error)
+      }
+    } else {
+      console.log(localSavedCurrency.currency + " is not under our threshold");
+    }
+  });
+  console.log("    ");
+}, 5000);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
